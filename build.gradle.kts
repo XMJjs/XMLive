ec9a88f213edb66b5f861c8085077c3f5f3f4e10
XMJ <XMJ_js@163.com>
2026-04-10 18:15:52 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,31 @@
-// build.gradle.kts
-// import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     java
 }
 
-group = "com.xmjjs"      // 替换成你的包名
-version = "1.0.0"              // 你的插件版本
+group = "com.xmjjs"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
-    // PaperMC 仓库，用于获取 Paper API
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.codemc.io/repository/maven-releases/")
 }
 
 dependencies {
-    // Paper API 1.21+，编译时依赖（服务器已自带，所以用 compileOnly）
-    // 确保版本与你的服务器版本匹配
-    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
+    compileOnly("com.github.retrooper:packetevents-spigot:2.11.2")
 }
 
 java {
-    // 使用 Java 21，Paper 1.21+ 要求
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 tasks {
     processResources {
-        // 在构建时自动将 version 写入 plugin.yml
         filesMatching("plugin.yml") {
             expand(project.properties)
         }
     }
-
-    // 打包时的 JAR 命名
     jar {
         archiveFileName.set("XMLIVE-${project.version}.jar")
     }

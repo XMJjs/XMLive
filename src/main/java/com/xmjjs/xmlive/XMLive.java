bf1445b21e2044a4b17d16bce29f28ba106f861b
XMJ <XMJ_js@163.com>
2026-04-10 17:02:14 +0800

    Update XMLive.java

--- src/main/java/com/xmjjs/xmlive/XMLive.java
+++ src/main/java/com/xmjjs/xmlive/XMLive.java
@@ -1,4 +1,3 @@
-// src/main/java/com/yourpackage/xmlive/XMLive.java
 package com.xmjjs.xmlive;
 
 import com.xmjjs.xmlive.auth.AuthManager;
@@ -19,32 +18,24 @@ public final class XMLive extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-
-        // 1. 保存默认配置文件
         saveDefaultConfig();
 
-        // 2. 初始化管理器
         configManager = new ConfigManager(this);
         authManager = new AuthManager(this);
         liveCore = new LiveCore(this);
 
-        // 3. 注册事件监听器
         getServer().getPluginManager().registerEvents(new CameraFollowListener(this), this);
         getServer().getPluginManager().registerEvents(new RecorderRestrictListener(this), this);
 
-        // 4. 注册命令
-        getCommand("xl").setExecutor(new XLCommand(this));
-        // 在 XMLive.java 的 onEnable() 方法中添加
-		XLCommand xlCommand = new XLCommand(this);
-		getCommand("xl").setExecutor(xlCommand);
-		getCommand("xl").setTabCompleter(xlCommand);
+        XLCommand xlCommand = new XLCommand(this);
+        getCommand("xl").setExecutor(xlCommand);
+        getCommand("xl").setTabCompleter(xlCommand);
 
         getLogger().info("XMLIVE 插件已成功启动！");
     }
 
     @Override
     public void onDisable() {
-        // 关闭所有自动切换任务，释放资源
         if (liveCore != null) {
             liveCore.shutdown();
         }
